92c9dedf0f495b17f712bb4f584f1d5ff9698196
Lucas Camelo <lucas_lcds@hotmail.com>
2026-07-23 22:11:32 -0300

    aula 4 react iteracao sobre arrays com map

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Pai from './components/Pai';
+import ItemList from './components/aula04/itemList';
+import ListaProdutos from './components/aula04/ListaProdutos'
+import ListaDeProdutos from './components/aula04/ListaProdutos';
 
 function App() {
   return (
     <div>
+      <ItemList />
       <Pai />
+      <ListaDeProdutos />
     </div>
   );
 }
