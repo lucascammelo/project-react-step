1b4abc4bdbc8963a1422c5ac958e8017ea19761d
Lucas Camelo <lucas_lcds@hotmail.com>
2026-08-11 21:25:12 -0300

    aula 9 formularios react

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,16 +1,14 @@
 
-import Usuarios from './components/aula08/Usuarios';
-import Posts from './components/aula08/Posts';
-import ListaUsuarios from './components/aula08/ListaUsuarios';
+import RegistrationForm from "./components/aula09/RegistrationForm";
+
 function App() {
   return (
     <div>
-      <h1>JSONPlaceholder App</h1>
-      <Usuarios />
-      <Posts />
-      <ListaUsuarios />
+      <h1>Formulário de Cadastro</h1>
+      <RegistrationForm />
     </div>
   );
 }
+
 export default App;
           
