d98ad52a10da28afc8d9918d2937622f066d4057
Lucas Camelo <lucas_lcds@hotmail.com>
2026-09-07 06:42:33 -0300

    feito o deletar

--- my-react-app/src/components/aulas integradas 2/Home.jsx
+++ my-react-app/src/components/aulas integradas 2/Home.jsx
@@ -9,25 +9,47 @@ function Home() {
     const navigate = useNavigate();
 
 
-    useEffect(() => {
+    const deletarProduto = async (id) => {
+        try{
+            const token = localStorage.getItem('token');
+            await axios.delete(
+                `https://projeto-node-step-t5i1.vercel.app/produtos/${id}`,
+                {
+                    headers: { Authorization: `Bearer ${token}` },
+                }
+
+            );
+            carregarProdutos();
+        }catch (err) {
+            console.error('Erro ao deletar', err)
+            alert('Erro ao deletar o produto')
+        }
+    }
+
+
+    const carregarProdutos = () => {
         axios.get('https://projeto-node-step-t5i1.vercel.app/produtos')
             // "then" - quando a resposta chegar, faça isso...
             .then((response) => {
                 setProdutos(response.data);// o 'data' é especifico do AXIOS, é o que contem o conteudo da api
-        })
+            })
+    };
+
+    useEffect(() => {
+        carregarProdutos();
     }, [])
 
     return (
         <div>
             <h1>LIsta de Produtos</h1>
             <ul>
-                {produtos.map( (produtoMap) => (
+                {produtos.map((produtoMap) => (
 
                     <li key={produtoMap._id}>
-                        {produtoMap.nome} - {produtoMap.preco} - {produtoMap.descricao} - {produtoMap.avaliacao.nota} <button onClick={() => navigate(`/DeletarProduto/${produtoMap._id}`)}>Deletar</button>
+                        {produtoMap.nome} - {produtoMap.preco} - {produtoMap.descricao} - {produtoMap.avaliacao.nota} <button onClick={() => deletarProduto(produtoMap._id)}>Deletar</button>
                         <button onClick={() => navigate(`/EditarProduto/${produtoMap._id}`)}>Editar</button>
                     </li>
-                ) )}
+                ))}
             </ul>
         </div>
 
